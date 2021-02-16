aff91b69a1ff55ebee0293f8e7882d4f57807195
= <=>
2021-02-16 17:46:21 +0700

    MyApp text Berita Hari ini

--- lib/main.dart
+++ lib/main.dart
@@ -4,66 +4,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Contoh GridView Widget"),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: <Widget>[
-            Container(
-              color: Colors.green,
-              alignment: Alignment.center,
-              child: Text("1",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-            ),
-            Container(
-              color: Colors.red,
-              alignment: Alignment.center,
-              child: Text("2",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 400.0,
-              width: 300.0,
-            ),
-            Container(
-              color: Colors.deepPurple,
-              alignment: Alignment.center,
-              child: Text("3",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-            Container(
-              color: Colors.blueGrey,
-              alignment: Alignment.center,
-              child: Text("4",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-            Container(
-              color: Colors.cyan[800],
-              alignment: Alignment.center,
-              child: Text("5",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-            Container(
-              color: Colors.yellow[800],
-              alignment: Alignment.center,
-              child: Text("6",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-          ],
-        ),
-      ),
+          appBar: AppBar(title: Text("MyApp"), backgroundColor: Colors.red),
+          body: Container(
+              alignment: Alignment.topCenter,
+              child: Text(
+                'BERITA TERBARU     PERTANDINGAN HARI INI',
+                style: TextStyle(fontSize: 15, height: 3.0),
+              ))),
     );
   }
 }
